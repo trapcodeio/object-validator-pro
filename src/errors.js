761f981437642ed087c238631a6ef09e8d7d6d65
Evil <lordcashtugbaski@gmail.com>
2019-03-12 19:20:38 +0100

    Ready for documentation!

--- src/errors.js
+++ src/errors.js
@@ -1,6 +1,7 @@
 let errors = {
     default: ':param returned :false',
     must: ':param is required.',
+    typeOf: ':param is not typeOf :option',
     min: ':param is too small. (Min. :option)',
     max: ':param is too big. (Max. :option)',
     minLength: ':param is too short. (Min. :option characters)',
